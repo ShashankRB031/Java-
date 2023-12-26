e8e7a2ca8e3485180d739ae6887f7efddee024c5
Shashank R B <152292533+ShashankRB031@users.noreply.github.com>
2023-12-26 14:56:17 +0530

    SRB

--- LeapYear.java
+++ LeapYear.java
@@ -1,4 +1,4 @@
-//Program that takes a year from user and print whether that year is a leap year or not. SRB 
+//Program that takes a year from user and print whether that year is a leap year or not.
 import java.util.Scanner;
 
 public class LeapYear
